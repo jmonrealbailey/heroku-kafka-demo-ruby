0bab2f55faf1f093d48bbff5359e3bc1a9b75674
José Monreal Bailey <jmonrealbailey@salesforce.com>
2024-07-03 11:34:54 +0100

    Changing env var to addons env var

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ end
 
 def initialize_kafka
   tmp_ca_file = Tempfile.new('ca_certs')
-  tmp_ca_file.write(ENV.fetch('HEROKU_KAFKA_JIMONREAL_TRUSTED_CERT'))
+  tmp_ca_file.write(ENV.fetch('HEROKU_KAFKA_V2_8_TRUSTED_CERT'))
   tmp_ca_file.close
 
   # This demo app connects to kafka on multiple threads.
@@ -23,20 +23,20 @@ def initialize_kafka
   # for the producer and a different one for the consumer.
   #
   producer_kafka = Kafka.new(
-    seed_brokers: ENV.fetch('HEROKU_KAFKA_JIMONREAL_URL'),
+    seed_brokers: ENV.fetch('HEROKU_KAFKA_V2_8_URL'),
     ssl_ca_cert_file_path: tmp_ca_file.path,
-    ssl_client_cert: ENV.fetch('HEROKU_KAFKA_JIMONREAL_CLIENT_CERT'),
-    ssl_client_cert_key: ENV.fetch('HEROKU_KAFKA_JIMONREAL_CLIENT_CERT_KEY'),
+    ssl_client_cert: ENV.fetch('HEROKU_KAFKA_V2_8_CLIENT_CERT'),
+    ssl_client_cert_key: ENV.fetch('HEROKU_KAFKA_V2_8_CLIENT_CERT_KEY'),
     ssl_verify_hostname: false,
   )
 
   $producer = producer_kafka.async_producer(delivery_interval: 1)
 
   consumer_kafka = Kafka.new(
-    seed_brokers: ENV.fetch('HEROKU_KAFKA_JIMONREAL_URL'),
+    seed_brokers: ENV.fetch('HEROKU_KAFKA_V2_8_URL'),
     ssl_ca_cert_file_path: tmp_ca_file.path,
-    ssl_client_cert: ENV.fetch('HEROKU_KAFKA_JIMONREAL_CLIENT_CERT'),
-    ssl_client_cert_key: ENV.fetch('HEROKU_KAFKA_JIMONREAL_CLIENT_CERT_KEY'),
+    ssl_client_cert: ENV.fetch('HEROKU_KAFKA_V2_8_CLIENT_CERT'),
+    ssl_client_cert_key: ENV.fetch('HEROKU_KAFKA_V2_8_CLIENT_CERT_KEY'),
     ssl_verify_hostname: false,
   )
 
